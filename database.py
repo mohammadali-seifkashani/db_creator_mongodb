7d0e676033a905f9c9c4b0ba0e4cbbaa3edfa679
mohammadali-seifkashani <seifkashani14@gmail.com>
2022-07-28 06:33:11 +0430

    implement getting database with getting tables (values)

--- database.py
+++ database.py
@@ -25,7 +25,7 @@ class MongoDB:
 
     @time_decorator
     def fill(self, baseaddr, columns):
-        existing_table_names = self.get_table_names()
+        existing_table_names = self.get_collection_names()
         if 'general' in columns:
             for i, data in enumerate(load_data(baseaddr, existing_table_names)):
                 print(i, data['table_name'])
@@ -41,7 +41,26 @@ class MongoDB:
                 json_data = [dict(zip(columns[data['table_name']], r)) for r in data['rows']]
                 self.fill_table(data['table_name'], json_data)
 
-    def remove(self, dbname):
+    @staticmethod
+    def remove(dbname):
         myclient = pymongo.MongoClient("mongodb://localhost:27017/")
         myclient.drop_database(dbname)
 
+    def get_collection_names(self):
+        return self.db.list_collection_names()
+
+    def get_collection(self, collection_name, columns=['*']):
+        if columns == ['*']:
+            yield from self.db[collection_name].find()
+        else:
+            columns_dict = {column:1 for column in columns}
+            columns_dict['_id'] = 0
+            yield from self.db[collection_name].find({}, columns_dict)
+
+    def get_database(self, columns):
+        result = {}
+        collection_names = self.get_collection_names()
+        # self.cursor.itersize = 10000
+        for i, t in enumerate(collection_names):
+            print(i, t)
+            yield self.get_collection(t, columns)
